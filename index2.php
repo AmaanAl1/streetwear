0b09d48f11f06903822da6d94c9d484dfe781257
AmaanAl1 <aa19033@melville-high.school.nz>
2023-05-22 16:24:35 +1200

    gallery

---
+++ index2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Streetwear-Home</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/style2.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="wrapper">
+<div class="header">
+  <h1>Streetwear</h1>
+  <p>Clothing brand</p>
+</div>
+</div>
+<?php include "navbar.php"; ?>
+
+
+    
+<div id="slider">
+    <figure>
+    <img src="images/brown-pants.jpg">
+    <img src="images/blue-jeans.jpg">
+    <img src="images/black-t.jpg">
+    <img src="images/pink-hoodie.jpg">
+    <img src="images/brown-pants.jpg">
+    </figure>
+    </div>    
+    
+<div class="footer">
+  <h2>Footer</h2>
+</div>
+
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,6 @@
+<div class="navbar">
+        <a href = "menswear2.php">Menswear</a>
+        <a href = "womenswear.php">Womenswear</a>
+        <a href = "kids.php">Kids</a>
+  <a href="login.php" class="right">Log in Log out</a>
+</div>
