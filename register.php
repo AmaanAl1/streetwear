157143c1e5f4b620bcb8e812969942443eec2470
AmaanAl1 <aa19033@melville-high.school.nz>
2023-08-17 09:34:04 +1200

    login page and register page working

--- register.php
+++ register.php
@@ -30,16 +30,10 @@
 
 <?php
 // Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
+require_once 'setup.php';
+
+// We don't have the password or email info stored in sessions, so instead, we can get the results from the database.
+$stmt = $conn->prepare('SELECT password, email FROM accounts WHERE id = ?');
 
 // Now we check if the data was submitted, isset() function will check if the data exists.
 if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
@@ -72,12 +66,13 @@ if ($stmt = $conn->prepare('SELECT id, password FROM accounts WHERE username = ?
 		echo 'Username exists, please choose another!';
 	} else {
 		// Username doesn't exists, insert new account
-if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
+        if ($stmt = $conn->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
 	// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
 	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 	$stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
+
 	$stmt->execute();
-	echo 'You have successfully registered! You can now login!';
+    echo 'You have successfully registered, you can now login!';
 } else {
 	// Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all 3 fields.
 	echo 'Could not prepare statement!';
@@ -88,5 +83,5 @@ if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALU
 	// Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all 3 fields.
 	echo 'Could not prepare statement!';
 }
-$con->close();
+$conn->close();
 ?>
